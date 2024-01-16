19fa1a9ee8b91f79ff162eed360017964962469f
Jérémy Sorant <jsorant@gmail.com>
2024-01-16 12:52:16 +0100

    Added vitest mocks

--- src/Calculator.spec.ts
+++ src/Calculator.spec.ts
@@ -1,7 +1,10 @@
+import { describe } from "node:test";
 import { expect, test } from "vitest";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("Calculator", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 });
 
 function sum(a: number, b: number): number {
