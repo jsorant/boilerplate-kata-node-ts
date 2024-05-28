60d9418dbd0acb491d9122c064f55d2275deb0c6
Jérémy Sorant <jsorant@gmail.com>
2024-05-28 22:45:08 +0200

    use it instead of test

--- src/Tests.spec.ts
+++ src/Tests.spec.ts
@@ -1,7 +1,7 @@
-import { expect, test, describe } from "vitest";
+import {expect, it, describe} from "vitest";
 
 describe("Calculator", () => {
-  test("adding 1 and 2 should equal 3", () => {
-    expect(1 + 2).toBe(3);
-  });
+    it("adding 1 and 2 should equal 3", () => {
+        expect(1 + 2).toBe(3);
+    });
 });
