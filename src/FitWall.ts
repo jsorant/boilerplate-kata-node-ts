fc870b78c4e99b6a755e507a64876686329ec390
Jérémy Sorant <jsorant@gmail.com>
2024-06-02 11:22:00 +0200

    resolution

--- src/FitWall.ts
+++ src/FitWall.ts
@@ -5,41 +5,73 @@ export class Element {
     static of(width: number) {
         return new Element(width);
     }
+
+    copy() {
+        return new Element(this.width);
+    }
 }
 
 export class Combination {
-    private constructor(public readonly elements: Element[]) {
+    private constructor(private readonly elements: Element[]) {
     }
 
     static of(elementsWidths: number[]) {
-        return new Combination(elementsWidths.map(width => Element.of(width)));
+        return new Combination(elementsWidths.map(width => Element.of(width))
+            .sort((a, b) => a.width - b.width));
+    }
+
+    copy() {
+        return new Combination([...this.elements]);
+    }
+
+    hash() {
+        return [...this.elements]
+            .sort((a, b) => a.width - b.width)
+            .reduce((acc, current) => acc += `${current.width};`, "");
+    }
+
+    add(element: Element) {
+        this.elements.push(element);
     }
 }
 
-export function fitWall(wallWidth: number, elements: Element[]): Combination[] {
+export class Combinations {
+    private map = new Map<string, Combination>();
 
-    let result = [];
+    add(combination: Combination) {
+        this.map.set(combination.hash(), combination);
+    }
+
+    values(): Combination[] {
+        return Array.from(this.map.values());
+    }
+}
+
+export function fitWall(wallWidth: number, elements: Element[]): Combinations {
+    let result: Combinations = new Combinations();
 
     for (const element of elements) {
-        let currentCombination = Combination.of([])
-
-        let remainingWall = wallWidth;
-
-        while (remainingWall >= element.width) {
-            currentCombination.elements.push(Element.of(element.width))
-            remainingWall -= element.width
-        }
-
-        for (const elementKey of elements) {
-            if (remainingWall >= elementKey.width) {
-                currentCombination.elements.push(Element.of(elementKey.width))
-                remainingWall -= elementKey.width
-            }
-        }
-
-        if (remainingWall === 0)
-            result.push(currentCombination);
+        addElement(elements, element, wallWidth, Combination.of([]), result);
     }
-    
-    return result
+
+    return result;
+}
+
+function addElement(elements: Element[], elementToAdd: Element, wallWidth: number, currentCombination: Combination, result: Combinations) {
+    if (wallWidth >= elementToAdd.width) {
+        currentCombination.add(elementToAdd.copy())
+    }
+
+    const remainingWidth = wallWidth - elementToAdd.width;
+
+    if (remainingWidth === 0) {
+        result.add(currentCombination);
+        return;
+    }
+
+    if (remainingWidth > 0) {
+        for (const element of elements) {
+            addElement(elements, element, remainingWidth, currentCombination.copy(), result);
+        }
+    }
 }
--- src/Tests.spec.ts
+++ src/Tests.spec.ts
@@ -1,43 +1,96 @@
 import {describe, expect, it} from "vitest";
-import {Combination, Element, fitWall} from "./FitWall";
+import {Combination, Combinations, Element, fitWall} from "./FitWall";
 
+describe("Combinasion", () => {
+    it("should have an hash based on elements", () => {
+        expect(Combination.of([50]).hash()).toEqual("50;")
+        expect(Combination.of([50, 50]).hash()).toEqual("50;50;")
+        expect(Combination.of([50, 100]).hash()).toEqual("50;100;")
+        expect(Combination.of([100, 50]).hash()).toEqual("50;100;")
+    })
+})
+
+
+describe("Combinasions", () => {
+    it("should add combination", () => {
+        const combinations = new Combinations();
+        combinations.add(Combination.of([50, 100]));
+
+        expectCombinationsAreInAnyOrder(
+            [Combination.of([50, 100])]
+            , combinations);
+    })
+
+    it("should have no duplicates", () => {
+        const combinations = new Combinations();
+
+        combinations.add(Combination.of([50, 100]));
+        combinations.add(Combination.of([50, 100]));
+        combinations.add(Combination.of([100, 50]));
+
+        expectCombinationsAreInAnyOrder(
+            [Combination.of([50, 100])]
+            , combinations);
+    })
+})
 
 describe("Wardrobe", () => {
     it("should fit a wall of 150 with elements : 50", () => {
         const combinations = fitWall(150, [Element.of(50)]);
 
-        expect(combinations).toStrictEqual(
+        expectCombinationsAreInAnyOrder(
             [Combination.of([50, 50, 50])]
-        )
+            , combinations);
     });
 
     it("should fit a wall of 100 with elements : 100, 50", () => {
         const combinations = fitWall(100, [Element.of(100), Element.of(50)]);
 
-        expect(combinations).toStrictEqual(
+        expectCombinationsAreInAnyOrder(
             [
-                Combination.of([100]),
-                Combination.of([50, 50])
+                Combination.of([50, 50]),
+                Combination.of([100])
             ]
-        )
+            , combinations);
+    });
+
+    it("should fit a wall of 110 with elements : 100, 50", () => {
+        const combinations = fitWall(110, [Element.of(100)]);
+
+        expect(combinations.values()).toStrictEqual([]);
     });
 
     it("should fit a wall of 150 with elements : 100, 50", () => {
         const combinations = fitWall(150, [Element.of(100), Element.of(50)]);
 
-        expect(combinations).toStrictEqual(
+        expectCombinationsAreInAnyOrder(
             [
-                Combination.of([100, 50]),
-                Combination.of([50, 50, 50])
+                Combination.of([50, 50, 50]),
+                Combination.of([100, 50])
             ]
-        )
+            , combinations);
     });
 
-    it("should fit a wall of 110 with elements : 100, 50", () => {
-        const combinations = fitWall(110, [Element.of(100), Element.of(50)]);
+    it("should fit a wall of 250 elements : 120, 100, 75, 50", () => {
+        const combinations = fitWall(250, [Element.of(120), Element.of(100), Element.of(75), Element.of(50)]);
 
-        expect(combinations).toStrictEqual(
-            []
-        )
+        expectCombinationsAreInAnyOrder(
+            [
+                Combination.of([50, 50, 50, 50, 50]),
+                Combination.of([100, 50, 50, 50]),
+                Combination.of([100, 100, 50]),
+                Combination.of([75, 75, 100]),
+                Combination.of([75, 75, 50, 50]),
+            ]
+            , combinations);
     });
 });
+
+function expectCombinationsAreInAnyOrder(expectedCombinations: Combination[], combinations: Combinations) {
+    expect(combinations.values().length).toBe(expectedCombinations.length);
+    expectedCombinations.forEach(expectedCombination => {
+        expect(combinations.values()).toContainEqual(
+            expectedCombination
+        );
+    })
+}
