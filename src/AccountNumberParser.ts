6f58f650b084251167eddd24689b77f27b3ec359
Jérémy Sorant <jsorant@gmail.com>
2024-07-14 14:43:52 +0200

    Introdule Entry

--- src/AccountNumberParser.ts
+++ src/AccountNumberParser.ts
@@ -6,43 +6,43 @@ const LINES_LENGTH = 27;
 const DIGITS_COUNT = 9;
 const DIGIT_WIDTH = 3;
 
-export class AccountNumberParser {
-    static toAccountNumber(entryLines: string[]) {
-        this.verifyLinesCount(entryLines);
-        this.verifyDigitLinesLength(entryLines);
-        this.verifyLastLineLength(entryLines);
-        return this.parseAccountNumber(entryLines);
+export class Entry {
+    constructor(private readonly lines: string[]) {
+        this.verifyLinesCount();
+        this.verifyDigitLinesLength();
+        this.verifyLastLineLength();
     }
 
-    private static verifyLinesCount(lines: string[]) {
-        if (lines.length !== LINES_COUNT)
-            throw new Error(`Invalid lines count, expected: ${LINES_COUNT}, received: ${(lines.length)}`);
+    toAccountNumber() {
+        let accountNumberValue = "";
+        for (let digitIndex = 0; digitIndex < DIGITS_COUNT; digitIndex++) {
+            const digitLines = this.digitsLines().map(l => l.slice(digitIndex * DIGIT_WIDTH, (digitIndex + 1) * DIGIT_WIDTH));
+            accountNumberValue += DigitParser.toNumber(digitLines)
+        }
+
+        return AccountNumber.of(accountNumberValue);
     }
 
-    private static verifyDigitLinesLength(lines: string[]) {
-        const found = this.digitsLines(lines).find(line => {
+    private verifyLinesCount() {
+        if (this.lines.length !== LINES_COUNT)
+            throw new Error(`Invalid lines count, expected: ${LINES_COUNT}, received: ${(this.lines.length)}`);
+    }
+
+    private verifyDigitLinesLength() {
+        const found = this.digitsLines().find(line => {
             return line.length !== LINES_LENGTH;
         });
         if (found)
             throw new Error(`Invalid line length, expected: ${LINES_LENGTH}, received: ${found.length}`)
     }
 
-    private static verifyLastLineLength(entryLines: string[]) {
-        let lastLine = entryLines.at(LINES_COUNT - 1)!;
+    private verifyLastLineLength() {
+        let lastLine = this.lines.at(LINES_COUNT - 1)!;
         if (lastLine.length !== 0)
             throw new Error(`Fourth line of the entry must be an empty line, found: '${lastLine}'`);
     }
 
-    private static parseAccountNumber(lines: string[]) {
-        let accountNumberValue = "";
-        for (let digitIndex = 0; digitIndex < DIGITS_COUNT; digitIndex++) {
-            const digitLines = this.digitsLines(lines).map(l => l.slice(digitIndex * DIGIT_WIDTH, (digitIndex + 1) * DIGIT_WIDTH));
-            accountNumberValue += DigitParser.toNumber(digitLines)
-        }
-        return AccountNumber.of(accountNumberValue);
-    }
-
-    private static digitsLines(lines: string[]) {
-        return lines.slice(0, LINES_COUNT - 1);
+    private digitsLines() {
+        return this.lines.slice(0, LINES_COUNT - 1);
     }
 }
